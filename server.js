cebdb693ac57d722595ebd4cb698c5a3ef123eba
hongvictoria <victoriahong96@gmail.com>
2016-10-22 11:34:38 -0700

    added login and registration

--- server.js
+++ server.js
@@ -21,6 +21,15 @@ app.get('/month', function (req, res) {
   // res.send('Hello World!');
 });
 
+app.get('/login', function (req, res) {
+  res.render('login',{});
+  // res.send('Hello World!');
+});
+
+app.get('/register', function (req, res) {
+	res.render('register', {});
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
